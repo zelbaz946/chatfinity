0c4f47732857448c02eaf8ef4b8da28c519c026a
Ziyad E. El Baz <ziyad.elbaz@me.com>
2021-03-10 13:36:17 -0600

    Puppeter is Working

--- tests/puppeteer.test.js
+++ tests/puppeteer.test.js
@@ -3,9 +3,17 @@ const chatfinity = `http://localhost:8080/`;
 import "regenerator-runtime/runtime.js";
 
 describe('Configuration Tests', () => {
+  let browser
+  let page
+
   beforeAll(async () => {
-    await page.goto(chatfinity);
-  });
+    browser = await puppeteer.launch();
+    page = await browser.newPage();
+  })
+
+  afterAll(() => {
+    browser.close();
+  })
 
   it('should be called Yack Yack Yack', async () => {
     await expect(page.title()).resolves.toMatch('Yack Yack Yack')
